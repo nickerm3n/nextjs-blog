9956d5c8cff35a75d12cdfb3203ec0180c76517b
Nikita Kurylko <nickermen13@gmail.com>
2020-08-02 22:16:48 +0300

    Fix: wrong path name in Date component

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,6 +1,6 @@
 import Layout from '../../components/layout'
 import { getAllPostIds, getPostData } from '../../lib/posts'
-import Date from '../../components/date'
+import Date from '../../components/Date'
 import utilStyles from '../../styles/utils.module.css'
 import Head from 'next/head'
 
